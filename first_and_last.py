f825cf38f8a06d4417c44b3dd3a025c752668bd9
Rishav poddar <eparis@atlassian.com>
2025-07-21 19:57:28 +0530

    first_and_last_occurence

--- first_and_last.py
+++ first_and_last.py
@@ -1,5 +1,4 @@
-
-def searchRange( nums,target):
+def searchRange( nums, target):
     li=[-1]*2
     low=0
     high=len(nums)-1
@@ -11,14 +10,12 @@ def searchRange( nums,target):
             last=mid
             
             if mid >0 and nums[mid-1]==target:
-                while first>0 and nums[first]==target:
+                while first>0 and nums[first-1]==target:
                     first-=1
             
             if mid < len(nums)-1 and nums[mid+1]==target:
-                while last<len(nums)-1 and nums[last] ==target:
+                while last<len(nums)-1 and nums[last+1] ==target:
                     last+=1
-            else:
-                last=len(nums)-1
             li[0]=first
             li[1]=last
             return li
@@ -28,8 +25,3 @@ def searchRange( nums,target):
             low=mid+1
     
     return li
-nums=[1,2,3,4,4]
-target=1
-print(searchRange(nums,target))
-
-                           
